4ca02a30d0008ad972966a7dfbc7cdf92c3ca443
Dmitry Vasilev <dmitry@t3dev.ru>
2022-11-03 19:31:24 +0300

    Add docker to project

---
+++ docker/docker-start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker start noproblem_web
+docker start noproblem_phpmyadmin
+docker start noproblem_mysql
+docker start noproblem_mailhog
+docker start noproblem_redis
+docker start noproblem_rabbit
---
+++ docker/docker-stop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker stop noproblem_web
+docker stop noproblem_phpmyadmin
+docker stop noproblem_mysql
+docker stop noproblem_mailhog
+docker stop noproblem_redis
+docker stop noproblem_rabbit
